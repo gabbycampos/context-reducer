350444b4d7e2c22084cc25c6c762905048331de2
Gabby Campos <gabbycampos@Gabbys-MacBook-Pro.local>
2020-05-31 15:55:37 -0500

    Updates

---
+++ precache-manifest.152a1cf75ab16850e193b7869d1e5690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8330a7ca9ba6260dbb1d46da5b0cd724",
+    "url": "/context-reducer/index.html"
+  },
+  {
+    "revision": "a460a4315845508f6870",
+    "url": "/context-reducer/static/css/main.d7aeeda9.chunk.css"
+  },
+  {
+    "revision": "2267ea2cf1130f731633",
+    "url": "/context-reducer/static/js/2.c3dd0e8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/context-reducer/static/js/2.c3dd0e8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a460a4315845508f6870",
+    "url": "/context-reducer/static/js/main.1b6ff831.chunk.js"
+  },
+  {
+    "revision": "6c56c43ed7048f35292f",
+    "url": "/context-reducer/static/js/runtime-main.217373d0.js"
+  }
+]);
